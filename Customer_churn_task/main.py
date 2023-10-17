f2047948a248d1fc3a2d3230a5824f646045e5f2
AdhamEbaidd <AdhamEbaid1@gmail.com>
2023-10-17 20:39:19 +0300

    cleaned the repetitve code displaying stats for each model by adding a function

--- Customer_churn_task/main.py
+++ Customer_churn_task/main.py
@@ -88,32 +88,37 @@ x_test_estimatedSalary_reshaped = np.reshape(x_test[:,-1], (-1, 1))
 x_test_estimatedSalary_scaled = sc3.transform(x_test_estimatedSalary_reshaped)
 x_test[:,-1] = np.ravel(x_test_estimatedSalary_scaled)
 
+def display_stats(model,model_name):
+    print(model_name+"\n ")
+    accuracies = cross_val_score(estimator=model, X=x_train, y=y_train, cv=5)
+
+    print(" accuracy : {: .2f} %".format(accuracies.mean() * 100))
+    print(" standard deviation : {: .2f} %".format(accuracies.std() * 100))
+
+    scoring = make_scorer(f1_score)
+    f1_scores = cross_val_score(estimator=model, X=x_train, y=y_train, cv=5, scoring=scoring)
+    average_f1_score = f1_scores.mean()
+
+    print("Average F1 score:", average_f1_score)
+
+    y_pred = model.predict(x_test)
+
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+    disp.plot()
+    plt.title(model_name)
+    plt.show()
+
+    print(accuracy_score(y_test, y_pred))
 
 
 """#Logistic Regression"""
 
-print("Logistic regression \n ")
+
 LR = LogisticRegression()
 LR.fit(x_train , y_train)
+display_stats(LR,"logistic regression")
 
-y_pred = LR.predict(x_test)
-
-
-cm=confusion_matrix(y_test,y_pred)
-
-disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=LR.classes_)
-disp.plot()
-plt.title("Logistic regression")
-plt.show()
-
-print(accuracy_score(y_test,y_pred))
-
-
-scoring = make_scorer(f1_score)
-f1_scores = cross_val_score(estimator=LR, X=x_train, y=y_train, cv=5, scoring=scoring)
-average_f1_score = f1_scores.mean()
-
-print("Average F1 score:", average_f1_score)
 
 """#KNN"""
 
@@ -121,115 +126,41 @@ print("K nearest neighbors \n ")
 KNN = KNeighborsClassifier(n_neighbors=19 , p=2)
 KNN.fit(x_train,y_train)
 
+display_stats(KNN,"K nearest neighbors")
 
-accuracies = cross_val_score(estimator=KNN , X =x_train , y=y_train , cv = 5 )
-
-print (" accuracy : {: .2f} %" .format(accuracies.mean()*100))
-print(" standard deviation : {: .2f} %" .format(accuracies.std()*100))
-
-
-scoring = make_scorer(f1_score)
-f1_scores = cross_val_score(estimator=KNN, X=x_train, y=y_train, cv=5, scoring=scoring)
-average_f1_score = f1_scores.mean()
-
-print("Average F1 score:", average_f1_score)
-
-y_pred_KNN = KNN.predict(x_test)
-
-
-cm=confusion_matrix(y_test,y_pred_KNN)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=KNN.classes_)
-disp.plot()
-plt.title("K nearest neighbors ")
-plt.show()
-
-print(accuracy_score(y_test,y_pred_KNN))
 
 """# SVM"""
 
-print("support vector machines \n ")
+
 
 SV_CL = SVC(kernel='poly')
 SV_CL.fit(x_train , y_train)
 
-accuracies = cross_val_score(estimator=SV_CL , X =x_train , y=y_train , cv = 5 )
-print ("accuracy : {: .2f} %" .format(accuracies.mean()*100))
-print("standard deviation : {: .2f} %" .format(accuracies.std()*100))
-
-scoring = make_scorer(f1_score)
-f1_scores = cross_val_score(estimator=SV_CL, X=x_train, y=y_train, cv=5, scoring=scoring)
-average_f1_score = f1_scores.mean()
-
-print("Average F1 score:", average_f1_score)
-
-y_pred_SVM = SV_CL.predict(x_test)
-cm=confusion_matrix(y_test,y_pred_SVM)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=SV_CL.classes_)
-disp.plot()
-plt.show()
-
-print(accuracy_score(y_test,y_pred_SVM))
+display_stats(SV_CL,"Support vector machines")
 
 """#Naive Bayes"""
-print(" Naive bayes \n ")
+
 
 NB = GaussianNB()
 NB.fit(x_train , y_train)
 
-accuracies = cross_val_score(estimator=NB , X =x_train , y=y_train , cv = 5 )
-print ("accuracy : {: .2f} %" .format(accuracies.mean()*100))
-print("standard deviation : {: .2f} %" .format(accuracies.std()*100))
-
-scoring = make_scorer(f1_score)
-f1_scores = cross_val_score(estimator=NB, X=x_train, y=y_train, cv=5, scoring=scoring)
-average_f1_score = f1_scores.mean()
-
-print("Average F1 score:", average_f1_score)
-
-y_pred_NB = NB.predict(x_test)
-cm=confusion_matrix(y_test,y_pred_NB)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=NB.classes_)
-disp.plot()
-plt.title("Naive bayes")
-plt.show()
-
-print(accuracy_score(y_test,y_pred_NB))
+display_stats(NB,"naive bayes classifier")
 
 """#Decision Tree"""
-print(" Decision tree \n ")
+
 
 DT = DecisionTreeClassifier(criterion="entropy")
 DT.fit(x_train , y_train)
 
-accuracies = cross_val_score(estimator=DT , X =x_train , y=y_train , cv = 5 )
-print ("accuracy : {: .2f} %" .format(accuracies.mean()*100))
-print("standard deviation : {: .2f} %" .format(accuracies.std()*100))
+display_stats(DT,"Decision trees")
 
 #from sklearn.tree import plot_tree
 #fig = plt.figure(figsize=(25,20))
 #_= plot_tree(DT , feature_names=data.columns,  class_names=["NO","YES"], filled=True , label= 'none', impurity= False)
 
 """#Random Forest"""
-print("Random forest \n ")
+
 
 RF = RandomForestClassifier(n_estimators = 200 , criterion= 'entropy' )
 RF.fit(x_train , y_train)
-
-accuracies = cross_val_score(estimator=RF , X =x_train , y=y_train , cv = 5 )
-print ("accuracy : {: .2f} %" .format(accuracies.mean()*100))
-print("standard deviation : {: .2f} %" .format(accuracies.std()*100))
-
-y_pred_RF = RF.predict(x_test)
-cm=confusion_matrix(y_test,y_pred_RF)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=NB.classes_)
-disp.plot()
-plt.title("Random forest")
-plt.show()
-
-scoring = make_scorer(f1_score)
-f1_scores = cross_val_score(estimator=RF, X=x_train, y=y_train, cv=5, scoring=scoring)
-average_f1_score = f1_scores.mean()
-
-print("Average F1 score:", average_f1_score)
-
-print(accuracy_score(y_test,y_pred_NB))
+display_stats(RF,"Random forest")
